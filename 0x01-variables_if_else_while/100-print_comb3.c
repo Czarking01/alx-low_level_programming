8bf83b1074d2f15d806ae766d11f220c4eb1cb61
Czarking01 <Czarking01@github.com>
2022-03-12 05:36:08 -0800

    Print combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - print combination of numbers
+ * main - Prints combination of numbers
  *
  * Return: Always (Success)
  */
 int main(void)
-{ 
+{
 	int c, i;
 
-	for (c = '0'; i <= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		for (i = '0'; i <= '9'; i++)
 		{
