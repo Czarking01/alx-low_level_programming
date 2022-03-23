b00c665107eef38139dc4f9011c9812d7ee95da9
Czarking01 <czarking01@github.com>
2022-03-23 01:52:05 +0000

    last commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,18 +7,10 @@
  */
 int main(void)
 {
-	int r = 0, c = 0;
-	time_t t;
+	int num;
 
-	srand((unsigned int) time(&t));
-	while (c < 2772)
-	{
-		r = rand() % 128;
-		if ((c + r) > 2772)
-			break;
-		c = c + r;
-		printf("%c", r);
-	}
-	printf("%c\n", (2772 - c));
+	srand(time(0));
+	num = rand();
+	printf("%i\n", num);
 	return (0);
 }
